10087079b33b8b86ba37f1496a9c9e79e98f6b55
Inbal <doar001@gmail.com>
2025-01-18 11:54:35 +0200

    center homepage text

--- src/components/HomeCollectionSection.tsx
+++ src/components/HomeCollectionSection.tsx
@@ -4,7 +4,7 @@ import ProductGridWrapper from "./ProductGridWrapper";
 const HomeCollectionSection = () => {
   return (
     <div>
-      <div className="max-w-screen-2xl flex items-center justify-between mx-auto mt-24 px-5 max-[400px]:px-3">
+      <div className="max-w-screen-2xl flex items-center justify-center mx-auto mt-24 px-5 max-[400px]:px-3">
         <h2 className="text-black text-5xl font-normal tracking-[1.56px] max-sm:text-4xl">
           המוצרים לשבוע הקרוב
         </h2>
